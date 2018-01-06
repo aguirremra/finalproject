d15424f38588ed1c34c145267e6623edd999f8a0
Zazil Toma <zaziltoma@gmail.com>
2018-01-06 14:18:06 -0800

    users and products model files ready

--- models/users.js
+++ models/users.js
@@ -1,24 +1,12 @@
-// Dependencies
-// =============================================================
-// References the standard library:
-var Sequelize = require("sequelize");
-// References our connection to the DB:
-var sequelize = require("../config/connection.js");
-// Creates a "Chirp" model that matches up with DB
-var User = sequelize.define("user", {
-  name: {
-    type: Sequelize.STRING
-  },
-  sub: {
-    type: Sequelize.STRING
-  },
-  created_at: {
-    type: Sequelize.DATE
-  }
-}, {
-  timestamps: false
-});
-// Syncs with DB
-Chirp.sync();
-// Makes the Chirp Model available for other files (will also create a table)
-module.exports = Chirp;
+console.log('models/users is loaded');
+module.exports = function(sequelize, DataTypes){
+  var User = sequelize.define("User", {
+    
+    sub: {
+      type: DataTypes.STRING
+    };
+
+  });
+
+  return User;
+};
