d10b2e9a7d81726f0c7bc095f8580891946d305b
Dan Moneypenny <catullus1973@gmail.com>
2018-01-10 20:19:25 -0800

    Deleted oldfiles folder

--- client/~oldfiles/oldindex.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import { makeMainRoutes } from './routes';
-import registerServiceWorker from './registerServiceWorker';
-
-const routes = makeMainRoutes();
-
-ReactDOM.render(routes, document.getElementById('root'));
-registerServiceWorker();
-
