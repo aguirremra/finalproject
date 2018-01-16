1669bafab7d8333b86c63b54c726bc420138f784
Dan Moneypenny <catullus1973@gmail.com>
2018-01-15 21:27:55 -0800

    Deleted obsolete component folders: Access, Container, Footer, Header, Nav

--- client/src/components/Access/Access.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Access = props =>
-  <div className="access" />;
-
-export default Access;
--- client/src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Header = props => <header className="container">{props.children}</header>;
-
-export default Header;
