0f463f2c24a29c8c58072cdfb7690f1b2eea4224
Dan Moneypenny <catullus1973@gmail.com>
2018-01-16 01:43:53 -0800

    Added files to People folder

---
+++ client/src/pages/People/People.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '.People.css';
+
+
+class People extends Component {
+
+  render() {
+
+    return (
+      <div>
+   
+    <div className="container mt-5">
+
+      <div className="row">
+
+        <div className="col-12">
+
+          <h2 className="text-center">Search for People</h2>
+
+          <p className="text-center">[[search field here]]</p>
+
+
+        </div>
+
+
+      </div>
+
+
+
+    
+    </div> 
+
+ 
+      </div>
+    );
+  }
+}
+
+export default People;
