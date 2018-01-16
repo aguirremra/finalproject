cbbacc3c5f386a85b30783f7d0ff3ab4174120db
Dan Moneypenny <catullus1973@gmail.com>
2018-01-16 00:44:25 -0800

    Added files to Land folder

---
+++ client/src/pages/Land/Land.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Container from '../../components/Containers/Container';
+import './Land.css';
+
+class Land extends Component {
+
+  render() {
+
+    return (
+
+        <div className="jumbotron mt-5">
+          <Container width="container">
+            <h1 className="display-4">Welcome to LiT List!</h1>
+            <p className="lead">This is the landing page for those who havent' signed up or logged in yet.</p>
+          </Container>
+        </div>
+      
+    );
+  }
+}
+
+export default Land;
