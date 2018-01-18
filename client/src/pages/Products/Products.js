a6eb1cb49fe7337bf44c331a1d6d3d407117811a
Enrico Bernardo <ebernardo@gmail.com>
2018-01-17 21:47:45 -0800

    added guard rails for Amazon searches

--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -26,15 +26,15 @@ class Products extends Component {
       .getProducts(this.state.searchString)
       .then((res) => {
         console.log("RESPONSE - AmazonProduct: ", res.data.Item);
-        console.log("Product Brand:", res.data.Item[itemNum].ItemAttributes[0].Brand[0]);
-        console.log("Product Title:", res.data.Item[itemNum].ItemAttributes[0].Title[0]);
-        console.log("List Price:", res.data.Item[itemNum].ItemAttributes[0].ListPrice[0].FormattedPrice[0]);
-        console.log("UPC:", res.data.Item[itemNum].ItemAttributes[0].UPC[0]);
-        console.log("Category:", res.data.Item[itemNum].ItemAttributes[0].Binding[0]);
-        console.log("Small Image:", res.data.Item[itemNum].SmallImage[0].URL[0]);
-        console.log("Medium Image:", res.data.Item[itemNum].MediumImage[0].URL[0]);
-        console.log("Large Image:", res.data.Item[itemNum].LargeImage[0].URL[0]);
-        console.log("URL: ", res.data.Item[itemNum].ItemLinks[0].ItemLink[0].URL[0]); 
+        // console.log("Product Brand:", res.data.Item[itemNum].ItemAttributes[0].Brand[0]);
+        // console.log("Product Title:", res.data.Item[itemNum].ItemAttributes[0].Title[0]);
+        // console.log("List Price:", res.data.Item[itemNum].ItemAttributes[0].ListPrice[0].FormattedPrice[0]);
+        // console.log("UPC:", res.data.Item[itemNum].ItemAttributes[0].UPC[0]);
+        // console.log("Category:", res.data.Item[itemNum].ItemAttributes[0].Binding[0]);
+        // console.log("Small Image:", res.data.Item[itemNum].SmallImage[0].URL[0]);
+        // console.log("Medium Image:", res.data.Item[itemNum].MediumImage[0].URL[0]);
+        // console.log("Large Image:", res.data.Item[itemNum].LargeImage[0].URL[0]);
+        // console.log("URL: ", res.data.Item[itemNum].ItemLinks[0].ItemLink[0].URL[0]); 
         // if (!res.data.results) return; 
 
         let returns = [];
@@ -64,13 +64,12 @@ class Products extends Component {
            <ResultsProducts
             brand={product.ItemAttributes[0].Brand[0]}
             title={product.ItemAttributes[0].Title[0]}
-            price={product.ItemAttributes[0].ListPrice[0].FormattedPrice[0]}
-            upc={product.ItemAttributes[0].UPC}
-            category={product.ItemAttributes[0].Binding}
-            img={product.MediumImage[0].URL}
-            purchase_link={product.ItemLinks[0].ItemLink[0].URL[0]}
+            price={product.ItemAttributes[0].ListPrice ? product.ItemAttributes[0].ListPrice[0].FormattedPrice[0] : "expen$ive"}
+            upc={product.ItemAttributes[0].UPC ? product.ItemAttributes[0].UPC : "xoxoxoxoxo"}
+            category={product.ItemAttributes[0].Binding ? product.ItemAttributes[0].Binding : "no Category"}
+            img={product.MediumImage ? product.MediumImage[0].URL : "http://i1.wp.com/williamlobb.com/wp-content/uploads/2017/10/amazon-frown.jpeg"}
+            purchase_link={product.ItemLinks[0].ItemLink ? product.ItemLinks[0].ItemLink[0].URL[0] : "http://www.amazon.com"}
             key={i}
-            product_id={"result_" + (i +1)}
           />
         )
       })
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -124,14 +124,15 @@ const controller = function() {
               productResponse = xml2JSresult;
           });
           console.log('RESPONSE - AmazonProduct', productResponse);
-          console.log("Product Brand:", productResponse.ItemSearchResponse.Items[0].Item[1].ItemAttributes[0].Brand[0]);
-          console.log("Product Title:", productResponse.ItemSearchResponse.Items[0].Item[1].ItemAttributes[0].Title[0]);
-          console.log("List Price:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].ListPrice[0].FormattedPrice[0]);
-          console.log("UPC:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].UPC[0]);
-          console.log("Category:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].Binding[0]);
-          console.log("Small Image:", productResponse.ItemSearchResponse.Items[0].Item[0].SmallImage[0].URL[0]);
-          console.log("Medium Image:", productResponse.ItemSearchResponse.Items[0].Item[2].MediumImage[0].URL[0]);
-          console.log("Large Image:", productResponse.ItemSearchResponse.Items[0].Item[2].LargeImage[0].URL[0]);
+          console.log("type response data: ",typeof productResponse);
+          // console.log("Product Brand:", productResponse.ItemSearchResponse.Items[0].Item[1].ItemAttributes[0].Brand[0]);
+          // console.log("Product Title:", productResponse.ItemSearchResponse.Items[0].Item[1].ItemAttributes[0].Title[0]);
+          // console.log("List Price:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].ListPrice[0].FormattedPrice[0]);
+          // console.log("UPC:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].UPC[0]);
+          // console.log("Category:", productResponse.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].Binding[0]);
+          // console.log("Small Image:", productResponse.ItemSearchResponse.Items[0].Item[0].SmallImage[0].URL[0]);
+          // console.log("Medium Image:", productResponse.ItemSearchResponse.Items[0].Item[2].MediumImage[0].URL[0]);
+          // console.log("Large Image:", productResponse.ItemSearchResponse.Items[0].Item[2].LargeImage[0].URL[0]);
           res.json(productResponse.ItemSearchResponse.Items[0]);
         })
       .catch(
