3bac4bfc64bfb640361cd3914b3ef9809a26e131
Zazil Toma <zaziltoma@gmail.com>
2018-01-20 13:26:35 -0800

    started modal for comment

---
+++ client/src/components/commentModal/commentModal.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return(
+      <div class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Add to LiTLiST</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">With textarea</span>
+            </div>
+            <textarea class="form-control" aria-label="With textarea"></textarea>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  }//render
+
+}//commentModal
+
+export default commentModal;
