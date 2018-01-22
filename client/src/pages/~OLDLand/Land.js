67594337379b6b55de53ee49b2e92d4cbf556186
Dan Moneypenny <catullus1973@gmail.com>
2018-01-22 15:47:05 -0800

    Added new landing page

---
+++ client/src/pages/~OLDLand/Land.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Container from '../../components/Containers/Container';
+import './Land.css';
+
+class Land extends Component {
+
+  render() {
+
+    return (
+
+        <div className="jumbotron mt-5">
+          <Container width="container">
+            <h1 className="display-4">Welcome to LiT List!</h1>
+            <h2>Landing Page</h2>
+            <p className="lead">This is the landing page for those who haven't signed up or logged in yet.</p>
+          </Container>
+        </div>
+      
+    );
+  }
+}
+
+export default Land;
