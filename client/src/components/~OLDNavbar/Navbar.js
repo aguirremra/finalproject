a22e94e0065c2aea9c481a371c53665cd457ded1
Dan Moneypenny <catullus1973@gmail.com>
2018-01-22 15:28:53 -0800

    Added new navbar

---
+++ client/src/components/~OLDNavbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Container from '../Containers/Container';
+import './Navbar.css';
+
+class Navbar extends Component {
+	render() {
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<Container width="container">
+			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+			    <span className="navbar-toggler-icon"></span>
+			  </button>
+			  <div className="collapse navbar-collapse" id="navbarText">
+			    <ul className="navbar-nav mr-auto">
+			      <li className="nav-item">
+			        <a className="nav-link" href="/home">Home </a>
+			      </li>
+			      <li className="nav-item">
+			        <a className="nav-link" href="/people">People</a>
+			      </li>
+			      <li className="nav-item">
+			        <a className="nav-link" href="/places">Places</a>
+			      </li>
+			      <li className="nav-item">
+			        <a className="nav-link" href="/products">Products</a>
+			      </li>
+			      <li className="nav-item">
+			        <a className="nav-link" href="/placesfav">Favorite Places</a>
+			      </li>
+			      <li className="nav-item">
+			        <a className="nav-link" href="/productsfav">Favorite Products</a>
+			      </li>				      				      			      
+			    </ul>
+			    <span className="navbar-text">
+			      Search for stuff. Save your favorites
+			    </span>
+			  </div>
+			 </Container>
+			</nav>		
+			);
+	}//render
+
+
+
+}//Navbar
+
+export default Navbar;
