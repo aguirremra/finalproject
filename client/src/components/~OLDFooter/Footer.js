5147a882b2950e4f942f080a954e4c159320ab26
Dan Moneypenny <catullus1973@gmail.com>
2018-01-22 15:04:48 -0800

    Added new footer design

---
+++ client/src/components/~OLDFooter/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Container from '../Containers/Container';
+import './Footer.css';
+import logo from '../../logo.svg';
+
+class Footer extends Component {
+
+  render() {
+
+    return (
+
+      <footer>
+        <Container width="container">
+
+          <p className="text-center">&copy; 2018 LiT List &#183; <a href="https://github.com/aguirremra/finalproject" target="_blank" rel="noopener noreferrer">GitHub</a> &#183; Powered by <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a><img src={logo} className="App-logo" alt="logo" />
+</p>
+
+        </Container>
+
+      </footer>
+      
+    );
+  }
+}
+
+export default Footer;
